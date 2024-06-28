299b5d7dad3cc0883b25374bb413e44cacdf8512
Joel O’Connor <18236403+joeloconnor-nz@users.noreply.github.com>
2024-06-28 15:04:44 +1200

    Improve form-schema.ts, Improve contact form layout

--- app/contact/form-schema.ts
+++ app/contact/form-schema.ts
@@ -6,12 +6,14 @@ export const formSchema = z.object({
     .trim()
     .min(2, { message: 'Name must contain at least 2 characters' })
     .max(50, { message: 'Name cannot be longer than 50 characters' }),
-  email: z.string().email({ message: 'You must enter an email' }),
+  email: z.string().email({ message: 'You must enter a valid email' }),
   message: z
     .string()
     .trim()
-    .min(10, { message: 'Message must contain at least 10 characters' })
-    .max(2000, { message: 'Message cannot be longer than 2000 characters' }),
+    .min(10, { message: 'Your message must contain at least 10 characters' })
+    .max(2000, {
+      message: 'Your message cannot be longer than 2000 characters',
+    }),
   captchaToken: z.string({
     message: 'Please complete the captcha verification',
   }),
