43922a30e84d77a3c2dda72dcc83a56667af5021
Joel O'Connor <18236403+joeloconnor-nz@users.noreply.github.com>
2025-04-27 18:27:22 +1200

    Remove Contentlayer2 and Blog (#15)
    Remove Contentlayer2 integration including blog page and files relating to posts

--- app/components/header-nav.tsx
+++ app/components/header-nav.tsx
@@ -4,8 +4,7 @@ import Link from 'next/link'
 export function HeaderNav() {
   return (
     <nav>
-      <ul className="flex gap-4">
-        <HeaderNavLink href="/blog">Blog</HeaderNavLink>
+      <ul>
         <HeaderNavLink href="/contact">Contact</HeaderNavLink>
       </ul>
     </nav>
--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-const { withContentlayer } = require('next-contentlayer2')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
-
-module.exports = withContentlayer(nextConfig)
