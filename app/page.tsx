e912d99914a22282a9ce65fa92500fb9928bb5fd
Joel O'Connor <18236403+joeloconnor-nz@users.noreply.github.com>
2025-04-28 22:43:30 +1200

    - Replaced the header with a more engaging introduction - Revised the description to reflect current learning in software development.

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,24 @@ export default function Home() {
   return (
     <div className="flex grow flex-col items-center justify-center gap-10 px-6 py-8 text-center">
       <Image
-        className="size-40 rounded-xl shadow-2xl shadow-stone-900/60 dark:shadow-stone-700/30 md:size-44"
+        className="size-40 rounded-xl shadow-2xl shadow-stone-900/60 md:size-44 dark:shadow-stone-700/30"
         src="/images/profile-photo.jpeg"
         width="176"
         height="176"
         alt="Image of Joel O'Connor"
         priority
       />
-      <h1 className="text-4xl font-medium text-stone-700 dark:text-stone-300 sm:text-5xl md:text-6xl">
-        Joel O&apos;Connor
-      </h1>
+      <div className="flex gap-1 font-sans text-4xl font-semibold">
+        <span className="text-blue-500">Hey!</span>
+        <span>&nbsp;I&apos;m Joel</span>
+        <span>👋</span>
+      </div>
       <div className="flex flex-col gap-2">
-        <p className="text-slate-600 dark:text-stone-400 md:text-xl">
-          Currently on the path path to become a software developer
+        <p className="text-slate-600 md:text-xl dark:text-stone-400">
+          I&apos;m learning software development—follow my journey
         </p>
-        <p className="hidden text-slate-600 dark:text-stone-400 sm:block md:text-xl">
-          Follow my progress on GitHub or connect with me on LinkedIn
+        <p className="hidden text-slate-600 sm:block md:text-xl dark:text-stone-400">
+          React, Next.js, TypeScript, Tailwind CSS, and more!
         </p>
       </div>
     </div>
