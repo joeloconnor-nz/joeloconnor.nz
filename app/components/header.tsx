5f7e9bcdc11c8f93674a4cb8618afaf5fc5a5a28
Joel O'Connor <18236403+joeloconnor-nz@users.noreply.github.com>
2025-04-30 09:54:15 +1200

    Conditionally render site title based on current pathname

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,17 +1,30 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { twMerge } from 'tailwind-merge'
 
 import { HeaderNav } from './header-nav'
 import { ThemeModeToggle } from './theme-mode-toggle'
 
 export function Header() {
+  const pathname = usePathname()
+
   return (
-    <header className="flex flex-row items-center justify-between p-6">
-      <Link
-        href="/"
-        className="text-xl font-light tracking-tight text-purple-500 transition-colors sm:text-2xl md:text-3xl dark:text-purple-600 dark:hover:text-stone-300"
-      >
-        joeloconnor.nz
-      </Link>
+    <header
+      className={twMerge(
+        'flex flex-row items-center p-6',
+        pathname === '/' ? 'justify-end' : 'justify-between',
+      )}
+    >
+      {pathname !== '/' && (
+        <Link
+          href="/"
+          className="text-xl font-light tracking-tight text-purple-500 transition-colors sm:text-2xl md:text-3xl dark:text-purple-600 dark:hover:text-stone-300"
+        >
+          joeloconnor.nz
+        </Link>
+      )}
       <div className="flex items-center gap-4">
         <HeaderNav />
         <ThemeModeToggle />
