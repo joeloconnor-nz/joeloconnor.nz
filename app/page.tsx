c3191ff36af5ca6595c7d1ebf49efc084571bdec
Joel O'Connor <18236403+joeloconnor-nz@users.noreply.github.com>
2025-05-06 20:28:46 +1200

    Update homepage blurb.

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,14 @@ export default function Home() {
       <div className="flex flex-col items-center gap-10 px-6 py-8 text-center">
         <ProfileImage />
         <AnimatedHeading />
-        <div className="flex flex-col gap-2">
-          <p className="text-slate-600 md:text-xl dark:text-stone-400">
-            I&apos;m learning software development—follow my journey
+        <div className="flex flex-col gap-2 text-slate-600 md:text-xl dark:text-stone-400">
+          <p>
+            I create AV & IT systems for large residential and marine projects,
           </p>
-          <p className="hidden text-slate-600 sm:block md:text-xl dark:text-stone-400">
-            React, Next.js, TypeScript, Tailwind CSS, and more!
+          <p className="mb-4">
+            while also expanding my expertise into software development.
           </p>
+          <p>Follow my socials below to see my journey!</p>
         </div>
       </div>
     </AnimatedLayout>
