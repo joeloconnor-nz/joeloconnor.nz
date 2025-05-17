b77f99c59235654f3ca80983766eb0df07c851c4
Joel O'Connor <18236403+joeloconnor-nz@users.noreply.github.com>
2025-05-18 00:31:46 +1200

    Update homepage spacing

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
       <div className="flex flex-col items-center gap-10 px-6 py-8 text-center">
         <ProfileImage />
         <AnimatedHeading />
-        <div className="flex flex-col gap-2 text-slate-600 sm:text-xl dark:text-stone-400">
+        <div className="flex max-w-xl flex-col gap-4 text-slate-600 sm:text-xl dark:text-stone-400">
           <p>
             I create AV & IT systems for large residential and marine projects,
-          </p>
-          <p className="mb-4">
+            <br />
             while also expanding my expertise into software development.
           </p>
+
           <p>Follow my socials below to see my journey!</p>
         </div>
       </div>
