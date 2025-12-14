4a7dff03f77ad37f13c48657248b7780cf5134cd
Joel O'Connor <18236403+joeloconnor-nz@users.noreply.github.com>
2025-12-14 16:38:41 +1300

    Upgrade to Zod 4 (#23)
    Upgraded `zod` from version 3.25.76 to 4.1.13.
    
    Changed `message` for `error`
    Changed `z.string().email()` for `z.email()`

--- app/contact/form-schema.ts
+++ app/contact/form-schema.ts
@@ -4,18 +4,18 @@ export const formSchema = z.object({
   name: z
     .string()
     .trim()
-    .min(2, { message: 'Name must contain at least 2 characters' })
-    .max(50, { message: 'Name cannot be longer than 50 characters' }),
-  email: z.string().email({ message: 'You must enter a valid email' }),
+    .min(2, { error: 'Name must contain at least 2 characters' })
+    .max(50, { error: 'Name cannot be longer than 50 characters' }),
+  email: z.email({ error: 'You must enter a valid email' }),
   message: z
     .string()
     .trim()
-    .min(10, { message: 'Your message must contain at least 10 characters' })
+    .min(10, { error: 'Your message must contain at least 10 characters' })
     .max(1000, {
-      message: 'Your message cannot be longer than 1000 characters',
+      error: 'Your message cannot be longer than 1000 characters',
     }),
   captchaToken: z.string({
-    message: 'Captcha verification failed. Please try again.',
+    error: 'Captcha verification failed. Please try again.',
   }),
 })
 
